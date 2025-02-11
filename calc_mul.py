b92b7dc49a21709f333957c94ad1e35d7a88947c
aajwtt <watanabe.shunsuke.zn@tut.jp>
2025-02-11 17:46:54 +0900

    Update calc_mul.py

--- calc_mul.py
+++ calc_mul.py
@@ -3,32 +3,35 @@
 import re
                 
 def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
-        else:
-                valid=False
+  ai=str(A)
+  bi=str(B)
+  p = re.compile('\d+(\.\d+)?')
+  if p.match(ai) and p.match(bi):
+    a=float(ai)
+    b=float(bi)
+    if a.is_integer() and b.is_integer():
+      if 1<=a<=999 and 1<=b<=999:
+        valid=True
+      else:
+        valid=False
+    else:
+      valid=False
+  else:
+    valid=False
                 
-        if valid:
-                ans=a*b
-                return ans
-        else:
-                return -1
+  if valid:
+    ans=a*b
+    return ans
+  else:
+    return -1
         
                 
 def main ():
-	matchstring = ''
-	while matchstring != 'end':
-                A = input ('input A: ')
-                B = input ('input B: ')
-                print ('input A * input B = ', calc(A,B))
+  matchstring = ''
+  while matchstring != 'end':
+    A = input ('input A: ')
+    B = input ('input B: ')
+    print ('input A * input B = ', calc(A,B))
 
 if __name__ == '__main__':
-	main()
+  main()
